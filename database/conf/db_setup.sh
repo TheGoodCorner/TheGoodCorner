2cbed4a060e9910316be04c30399404672a41c3a
Overtized <chanliatmaxence@gmail.com>
2026-08-04 08:56:32 +0200

    ajout health-check depend-on

--- database/conf/db_setup.sh
+++ database/conf/db_setup.sh
@@ -5,4 +5,5 @@ echo -e " Starting PostgreSQL initialization sequence up !"
 if [ -f "/run/secrets/db_password" ]; then
     export POSTGRES_PASSWORD=$(cat /run/secrets/db_password)
 fi
+
 exec docker-entrypoint.sh postgres	
