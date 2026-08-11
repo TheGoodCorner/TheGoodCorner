fd4f8bd70a24f09ec64bdd5bdd8df0d78363fca3
Overtized <chanliatmaxence@gmail.com>
2026-08-11 16:54:03 +0200

    ajout du get des produits de la product page

--- back/src/controllers/generalController.ts
+++ back/src/controllers/generalController.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
 import prisma from "../services/db.js";
 import { comparePassword } from "../utils/securityUtils.js";
-import multer from 'multer';
+import { ok } from "node:assert";
 // import express dependancies for request handling
 
-void multer;
 /**
  * generalController object creation with methods
  */
@@ -35,15 +34,37 @@ const generalController =
 		void req;
 		res.status(200).json({status: 'OK', message: 'profil page !'});
 	},
-	getProductsPage: (req: Request, res: Response) =>
+	getProductsPage: async (_req: Request, res: Response) =>
 	{
-		void req;
-		res.status(200).json({status: 'OK', message: 'product page !'});
+		try{
+			const products = await prisma.product.findMany();
+			return(res.status(200).json({status: 'OK', data:products}));
+		}
+		catch(error){
+			console.log(` an error ocurred inside the productPage getter` + error);
+			res.status(500).json({status: 'ERROR', message: 'failed to retrieve product', error: "Unknown error"});
+		}
 	},
 	getSignUpPage: async (req: Request, res: Response) =>
 	{
-		void req;
-		res.status(200).json({status: 'OK', message: 'signin page !'});
+		try
+		{
+			if(!req.body.email || !req.body.password) 
+			{
+				return res.status(400).json({status: 'ERROR', message: 'Email and password are required'});
+			}
+			const email = req.body.email;
+			const password = req.body.password;
+			let user = await prisma.user.findUnique({where:{email: email}});
+			console.log(user)
+			if(user && comparePassword(password, user.password))
+			{
+				return res.json({status: 'OK', user})
+			}
+			return res.json({status: 'NOT OK'})
+		}catch(error){
+			return (res.status(500).json({status: 'ERROR', message: 'Internal server error'}))
+		}
 	},
 }
 
