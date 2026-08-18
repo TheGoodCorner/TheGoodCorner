03d2db62e4445637eb049bdbac92f586f51aa9bd
Overtized <chanliatmaxence@gmail.com>
2026-08-18 12:03:01 +0200

    message template

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import path from 'path';
 import prisma from './services/db.js';
 
 import productRouter from './routes/products.js'
