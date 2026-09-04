a82dfbd67d2a27696d5f191892f5648b7133597b
maxence <maxence@2a01cb00843b030038daf26e26a68ef9.ipv6.abo.wanadoo.fr>
2026-09-04 11:24:08 +0200

    removed project name from stipe dockerfile, changed compose var to podman in makefile for compatibility

--- stripe/conf/stripe.sh
+++ stripe/conf/stripe.sh
@@ -4,6 +4,5 @@ set -e
 echo "[stripe_listener] Starting Stripe webhook listener..."
 
 exec stripe listen \
-    --api-key "$STRIPE_KEY" \
-    --forward-to "http://back:3000/newPayment/confirm" \
-    --project-name "thegoodcorner"
+    --api-key "$PRIVATE_STRIPE_KEY" \
+    --forward-to "http://back:3000/newPayment/confirm"
