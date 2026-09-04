b0c791268551e511093070f97784879636496e66
Delphine <delphine.pva@gmail.com>
2026-09-04 10:36:06 +0200

    FEAT: new button 'notification' added in navbar, no redirection yet

--- webserver/app/src/components/layouts/Navbar.jsx
+++ webserver/app/src/components/layouts/Navbar.jsx
@@ -4,7 +4,7 @@ import { useCartStore } from '../../stores/cartStore'
 import { useUIStore } from '../../stores/uiStore';
 import { Button } from '../UI/Button';
 import ProfileDropdown from '../profile/ProfileDropdown'
-import { Moon, Sun, ShoppingCart } from 'lucide-react'
+import { Moon, Sun, ShoppingCart, Bell } from 'lucide-react'
 
 function Navbar() {
   const theme = useThemeStore((state) => state.theme)
@@ -26,7 +26,6 @@ function Navbar() {
       />
       <div className="container">
         <div className="navbar-wrapper">
-          
           <ul className={`navbar-menu`}>
             <li><Link to="/" className="navbar-link">Accueil</Link></li>
             <li><Link to="/products" className="navbar-link">Produits</Link></li>
@@ -37,19 +36,29 @@ function Navbar() {
           </div>
         </div>
       </div>
+      <div className="flex items-center gap-2">
       <div className="relative">
-      <Button
-        onClick={() => {openUi('cart-popover')}}
-        variant='ghost'
-        icon={ShoppingCart}
-        title="Panier d'articles"
-        aria-label="Panier d'articles"
-      />
+        <Button
+          variant='ghost'
+          icon={Bell}
+          title="Notifications"
+          aria-label="Notifications"
+        />
+      </div>
+      <div className="relative">
+        <Button
+          onClick={() => {openUi('cart-popover')}}
+          variant='ghost'
+          icon={ShoppingCart}
+          title="Panier d'articles"
+          aria-label="Panier d'articles"
+        />
       {cartCount > 0 && (
         <div className="absolute -top-2 -right-2 bg-green-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs font-bold">
           {cartCount}
         </div>
       )}
+      </div>
       </div>
     </nav>
   );
